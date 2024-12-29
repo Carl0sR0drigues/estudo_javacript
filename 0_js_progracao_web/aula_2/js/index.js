1c1747f6e5fd30d406209dd17f53374885c30a08
Carl0sR0drigues <carlosrodrigues0100@gmail.com>
2024-12-29 02:26:29 -0300

    AUAL JS

---
+++ 0_js_progracao_web/aula_2/js/index.js
@@ -0,0 +1,6 @@
+n = document.getElementById('vem1')
+n.innerHTML = 'JS separado do HTML, usando variavel'
+
+document.getElementById('vem2').innerHTML = 'JS separado do HTML, sem usar variavel'
+
+alert('INFORMAÇÕES SOBRE JS')
