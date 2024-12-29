31111b68ae2ef3a071d30bd37e42fb4381c54022
Carl0sR0drigues <carlosrodrigues0100@gmail.com>
2024-12-29 14:18:08 -0300

    AUAL JS

---
+++ 0_js_progracao_web/aula_3/js/index.js
@@ -0,0 +1,11 @@
+n = document.getElementById("vem1")
+n.innerHTML = "JS separado do HTML, usando variavel"
+
+document.getElementById("vem2").innerHTML = "JS separado do HTML, sem usar variavel"
+
+document.getElementById("vem3").innerHTML = "Declarações são os dados desenvolvido em javascript e interpretado pelo navegador"
+
+document.getElementById("vem4").innerHTML = 
+"No javascript o que separa uma declaração de outro é 0 ';' permitindo quebrar linha se necessario, casso tenha uma quebra de linha sem ';' o JS considera a quebra o fim daquela declaração";
+
+alert('INFORMAÇÕES JS')
