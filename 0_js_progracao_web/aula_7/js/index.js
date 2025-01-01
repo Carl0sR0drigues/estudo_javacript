33877308d291ec037bafda60ca26bcf0ae16ee72
Carl0sR0drigues <carlosrodrigues0100@gmail.com>
2025-01-01 16:32:00 -0300

    AULA JS

--- 0_js_progracao_web/aula_7/js/index.js
+++ 0_js_progracao_web/aula_7/js/index.js
@@ -4,9 +4,9 @@ texto += 'OPERADORES<br>'
 
 document.getElementById('vemjs').innerHTML = texto
 
-document.getElementById('1').innerHTML = '1 - Operadores de Atribuição<br>'
-document.getElementById('2').innerHTML = '2 - Operadores Aritimeticos(Matematicos)<br>'
-document.getElementById('3').innerHTML = '3 - Operadores de Sequencia<br>'
-document.getElementById('4').innerHTML = '4 - Operadores de Comparação<br>'
+document.getElementById('1').innerHTML = '1 - Operadores Aritimeticos<br>'
+document.getElementById('2').innerHTML = '2 - Operadores atribuição<br>'
+document.getElementById('3').innerHTML = '3 - Operador Sequencial<br>'
+document.getElementById('4').innerHTML = '4 - Operadores comparação<br>'
 document.getElementById('5').innerHTML = '5 - Operadores Condicionais (Ternario)<br>'
 document.getElementById('6').innerHTML = '6 - Operadores Logicos<br>'
