84046687c4bc626dd0cdc037eaf9fb956c2d9602
Carl0sR0drigues <carlosrodrigues0100@gmail.com>
2025-01-02 00:08:59 -0300

    AULA JS

---
+++ 0_js_progracao_web/aula_8/js/index.js
@@ -0,0 +1,12 @@
+function somado(valor1, valor2) {
+    return valor1 + valor2;
+}
+
+function soma() {
+    var total = somado(10, 10); // Chama a função correta
+    alert(`Resultado da soma = ${total}`); // Exibe o resultado
+}
+
+
+
+
