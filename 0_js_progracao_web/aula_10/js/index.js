9ad8443b51da520446610805c9ed6d6b5bf4690d
Carl0sR0drigues <carlosrodrigues0100@gmail.com>
2025-01-02 21:28:07 -0300

    AULA JS

---
+++ 0_js_progracao_web/aula_10/js/index.js
@@ -0,0 +1,23 @@
+function onclick1(){
+    document.getElementById('a').style.backgroundColor = "blueviolet"
+}
+
+function onclick2(){
+    document.getElementById('a').style.backgroundColor = "black"
+}
+
+function dblclick1(){
+    document.getElementById('b').style.backgroundColor = "blueviolet"
+}
+
+function dblclick2(){
+    document.getElementById('b').style.backgroundColor = "black"
+}
+
+function onmouseover1(){
+    document.getElementById('c').style.backgroundColor = "blueviolet"
+}
+
+function onmouseover2(){
+    document.getElementById('c').style.backgroundColor = "black"
+}
