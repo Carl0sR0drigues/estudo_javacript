0fbde6b63990739e85848d5ef1bc75bc1ba03955
Carl0sR0drigues <carlosrodrigues0100@gmail.com>
2025-01-11 12:21:42 -0300

    AULA JS

---
+++ 0_js_progracao_web/aula_12/script.js
@@ -0,0 +1,18 @@
+function decimal(){
+    let numero = document.getElementById('numero').value;
+
+    if (numero < 10){
+        document.getElementById('resultado').innerHTML = 'O númeral é unidades';
+    } else if (numero >= 10 && numero < 100){
+        document.getElementById('resultado').innerHTML = 'O número é dezenas';
+    }
+    else if (numero < 1000 && numero >= 100){
+        document.getElementById('resultado').innerHTML = 'O número é centenas';
+    }
+    else if (numero >= 1000 && numero < 10000){
+        document.getElementById('resultado').innerHTML = 'O número é milhares';
+    }
+    else {
+        document.getElementById('resultado').innerHTML = 'O número é dezenas de milhares';
+    }
+}
